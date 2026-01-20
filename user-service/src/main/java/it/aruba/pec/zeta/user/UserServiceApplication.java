635434ec8079b36814f139841327c37a2b5ec463
QwertyKeyb0b <a.ferrulli27@studenti.uniba.it>
2026-01-20 17:24:10 +0100

    created userService main

---
+++ user-service/src/main/java/it/aruba/pec/zeta/user/UserServiceApplication.java
@@ -0,0 +1,12 @@
+package it.aruba.pec.zeta.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+}
