50d5a327d0d07c6b764abfc180abc6b5f8357480
QwertyKeyb0b <a.ferrulli27@studenti.uniba.it>
2026-01-24 19:13:52 +0100

    add PecServiceApplication.java

---
+++ pec-service/src/main/java/it/aruba/pec/zeta/pec/PecServiceApplication.java
@@ -0,0 +1,12 @@
+package it.aruba.pec.zeta.pec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PecServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PecServiceApplication.class, args);
+    }
+}
