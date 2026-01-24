c3a92a2455a097773619c9126d876146634d6cca
QwertyKeyb0b <a.ferrulli27@studenti.uniba.it>
2026-01-24 19:33:47 +0100

    fixed timestamp format to match mock sign.

--- common-lib/src/main/java/it/aruba/pec/zeta/common/dto/ArubaAuthChallengeDTO.java
+++ common-lib/src/main/java/it/aruba/pec/zeta/common/dto/ArubaAuthChallengeDTO.java
@@ -24,7 +24,7 @@ public class ArubaAuthChallengeDTO {
      * Timestamp generato dal server.
      * Usato per evitare replay attack (la firma scade dopo pochi minuti).
      */
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSSSS")
     private LocalDateTime timestamp;
 
     /**
